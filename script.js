f9f9906dc7dcca480561b4dde37addff521c1cc8
Ruthwika Prakash <sriruthwikaprakash786@gmail.com>
2026-06-05 14:45:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const searchBtn = document.getElementById("searchBtn");
+const weatherResult = document.getElementById("weatherResult");
 
+searchBtn.addEventListener("click", () => {
+
+    const city = document.getElementById("cityInput").value;
+
+    weatherResult.innerHTML = `
+        <h2>${city}</h2>
+        <p>Weather data will appear here.</p>
+    `;
+});
